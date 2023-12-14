5f04e3d67e4d4661ccc1e2248b37619c9357428d
Brad Traversy <traversymedia@gmail.com>
2023-12-14 14:04:01 -0500

    Format & sort by date

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export { formatDate };
