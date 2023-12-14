310107cba9c512436173b93740a1628e8f8545d7
Brad Traversy <traversymedia@gmail.com>
2023-12-14 12:15:43 -0500

    Blog collection schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,14 @@
-import { defineCollection, getCollection } from 'astro:content';
-const blogCollection = defineCollection({});
+import { defineCollection, z } from 'astro:content';
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.string(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
 
 export const collections = {
   blog: blogCollection,
