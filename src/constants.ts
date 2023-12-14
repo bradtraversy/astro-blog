10ac95d0852ee0ba15c278bd63930323da58069f
Brad Traversy <traversymedia@gmail.com>
2023-12-14 09:17:33 -0500

    Constants, navbar & footer

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'TechPeople Blog';
+export const SITE_DESCRIPTION =
+  'Articles, stories and tutorials from Tech People';
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
