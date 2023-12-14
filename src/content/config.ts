caf6c2d6219f3af411e6ad161a6170a586a15146
Brad Traversy <traversymedia@gmail.com>
2023-12-14 14:03:32 -0500

    Query articles for /articles

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ const blogCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    pubDate: z.string(),
+    pubDate: z.date(),
     author: z.string(),
     image: z.string(),
     tags: z.array(z.string()),
