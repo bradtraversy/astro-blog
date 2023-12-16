ced669aab087b0b0b8f1ec6288508c597be43aa8
Brad Traversy <traversymedia@gmail.com>
2023-12-15 19:08:02 -0500

    Pagination component

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = 'TechPeople Blog';
 export const SITE_DESCRIPTION =
   'Articles, stories and tutorials from Tech People';
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
+export const ARTICLES_PER_PAGE = 3;
